3d70fc63834c533123e902a309ac62fd287730aa
Weverton Carlos Leal de Oliveira <weverton.oliveira@itixti.com.br>
2020-05-11 15:52:38 -0300

    commit Inicial

---
+++ Report.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExemploWpfApp
+{
+	/// <summary>
+	/// Interaction logic for Report.xaml
+	/// </summary>
+	public partial class Report : Page
+	{
+		public Report()
+		{
+			InitializeComponent();
+		}
+
+		// Custom constructor to pass expense report data
+		public Report(object data) : this()
+		{
+			// Bind to expense report data.
+			this.DataContext = data;
+		}
+	}
+}
